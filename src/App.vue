93d057856f198c90fb16eace6f9ed7f46c20b346
Souvik Patra <souvik.patra@v2solutions.com>
2022-01-05 16:45:07 +0530

    Version 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo" />
+        <h1>Job Portal</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by Tittle</button>
+        <button @click="handleClick('location')">Order by Location</button>
+        <button @click="handleClick('salary')">Order by Salary</button>
+      </div>
+    </header>
+    <JobsList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from "vue";
+import Job from "./types/job";
+import OrderTerm from "./types/OrderTerm";
+import JobsList from "./components/JobsList.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    JobsList,
+  },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        id: "1",
+        title: "Software Engineer",
+        location: "Bengalore",
+        salary: 65000,
+      },
+      { id: "2", title: "Tester", location: "Mumbai", salary: 40000 },
+      { id: "3", title: "HR", location: "Kolkata", salary: 50000 },
+      { id: "4", title: "Team Lead", location: "Bengalore", salary: 82500 },
+      {
+        title: "Farm worker",
+        location: "lon ranch",
+        salary: 30000,
+        id: "5",
+      },
+      {
+        title: "Quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "6",
+      },
+      {
+        title: "Flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "7",
+      },
+      { title: "Fisherman", location: "lake hylia", salary: 21000, id: "8" },
+      {
+        title: "Prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "9",
+      },
+    ]);
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return {
+      jobs,
+      handleClick,
+      order,
+    };
+  },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+header h1 {
+  font-size: 3em;
 }
 </style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="job-list">
+    <transition-group name="list" tag="ul">
+      <li v-for="job in orderedJobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <img src="../assets/rupee.svg" alt="rupee icon">
+          <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          Lorem ipsum is a placeholder text commonly used to demonstrate the
+          visual form of a document or a typeface without relying on meaningful
+          content.
+        </div>
+      </li>
+    </transition-group>
+  </div>
+</template>
+<script lang="ts">
+import { computed, defineComponent, PropType } from "vue";
+import Job from "@/types/job";
+import OrderTerm from "@/types/OrderTerm";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>
+    }
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+
+    return {
+      orderedJobs,
+    };
+  },
+});
+</script>
+<style scoped>
+ .job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0
+  }
+  .job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
+  .list-move {
+    transition: all 1s;
+  }
+</style>
